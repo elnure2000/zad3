260ad80d126eb8fb6692d23d704e1500d6ee2692
Elnura Kubanychbekova <elnura.kubanychbekova.alt@gmail.com>
2022-06-16 14:47:32 +0600

    zad3

---
+++ script.js
@@ -0,0 +1,31 @@
+/*function str() {
+  let a = "555";
+  let b = "5";
+
+  console.log(Number(a) + Number(b));
+}
+
+str();*/
+
+/*function  evenNumber( a, b){
+let a;
+let b;
+if( a, b % 2===0 ){
+
+console.log(true);
+};
+{
+  console.log(false)
+ 
+}
+}
+
+evenNumber(20,99);*/
+
+const myMassuv = [2, 5, 9, 15, 0, 4];
+
+for (i = 0; i <= myMassuv.length; i++) {
+  if (myMassuv[i] > 2 && myMassuv[i] < 5) {
+    console.log(myMassuv[i]);
+  }
+}
